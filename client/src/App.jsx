43bf3b31eabbc6d518032c22a1acabdc08239d4e
DRAGONDINESH <lingamdinesh6@gmail.com>
2025-07-05 23:38:34 +0530

    Authentication Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,33 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
+import { SignIn, SignUp, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
 import OnboardingForm from './components/OnboardingForm';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <SignedIn>
-            <OnboardingForm />
-          </SignedIn>
-        }
-      />
-      <Route
-        path="/dashboard"
-        element={
-          <SignedIn>
-            <Dashboard />
-          </SignedIn>
-        }
-      />
-      <Route
-        path="*"
-        element={
-          <SignedOut>
-            <RedirectToSignIn />
-          </SignedOut>
-        }
-      />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn routing="path" path="/sign-in" />} />
+        <Route path="/sign-up" element={<SignUp routing="path" path="/sign-up" />} />
+        
+        <Route
+          path="/"
+          element={
+            <>
+              <SignedIn>
+                <Dashboard />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
+
+        <Route path="/onboarding" element={<OnboardingForm />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserButton } from '@clerk/clerk-react';
 import './Navbar.css';
 
 const Navbar = () => (
